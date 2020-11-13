d98294f7402c378805cd4a0fd318ef742f956a60
gyeonwoo <hoey777@naver.com>
2020-11-13 23:52:51 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          dfdfdf dfdfdfdf
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'img': 'https://placeimg.com/100/100/any',
+    'name': '이민아',
+    'birthday': '921212',
+    'gender': '여성',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'img': 'https://placeimg.com/100/100/any',
+    'name': '박병인',
+    'birthday': '961212',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'img': 'https://placeimg.com/100/100/any',
+    'name': '김성진',
+    'birthday': '891212',
+    'gender': '남자',
+    'job': '건설사'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      {
+        customers.map(c=>{
+          return (
+            <Customer 
+              id={c.id} 
+              img={c.img} 
+              name={c.name} 
+              birthday={c.birthday} 
+              gender={c.genter} 
+              job={c.job} 
+            />
+          )
+        })
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
